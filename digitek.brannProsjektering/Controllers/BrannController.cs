47acaa1917d737bf66377fba2a05fe31ce4251f2
Matias Gonzalez Tognoni <matias@arkitektum.no>
2018-10-12 12:11:37 +0200

    Add Submodel method to Controller

--- digitek.brannProsjektering/Controllers/BrannController.cs
+++ digitek.brannProsjektering/Controllers/BrannController.cs
@@ -1,9 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
+using CamundaClient;
+using digitek.brannProsjektering.Models;
+using CamundaClient.Requests;
+using CamundaClient.Service;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Newtonsoft.Json;
 
 namespace digitek.brannProsjektering.Controllers
 {
@@ -11,46 +20,137 @@ namespace digitek.brannProsjektering.Controllers
     [ApiController]
     public class DigiTekK11Controller : ControllerBase
     {
-        // GET: api/DigiTek17K11
-        /// <summary>
-        /// This shows the responce from Camunda server
-        /// </summary>
-        /// <returns></returns>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET: api/DigiTek17K11/5
         /// <summary>
         /// 
         /// </summary>
+        /// <param name="brannteknisk"></param>
+        /// <returns></returns>
+        // POST: api/DigiTek17K11
+        [HttpPost, Route("RisikoklassenModel")]
+        public IActionResult PostRkl([FromBody] BranntekniskProsjekteringModel brannteknisk)
+        {
+            var key = "RisikoklassenModel.Net";
+
+            var dictionary = brannteknisk.GetType()
+                .GetProperties(BindingFlags.Instance | BindingFlags.Public)
+                .ToDictionary(prop => prop.Name, prop => prop.GetValue(brannteknisk, null));
+            var camunda = new CamundaEngineClient();
+            var responce = camunda.BpmnWorkflowService.StartProcessInstance(key, dictionary);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return Ok(responce);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="brannteknisk"></param>
+        /// <returns></returns>
+        // POST: api/DigiTek17K11
+        [HttpPost, Route("BrannklasseModel")]
+        public IActionResult PostBKL([FromBody] BranntekniskProsjekteringModel brannteknisk)
+        {
+            var key = "BrannklasseModel.Net";
+
+            var dictionary = brannteknisk.GetType()
+                .GetProperties(BindingFlags.Instance | BindingFlags.Public)
+                .ToDictionary(prop => prop.Name, prop => prop.GetValue(brannteknisk, null));
+            var camunda = new CamundaEngineClient();
+            var responce = camunda.BpmnWorkflowService.StartProcessInstance(key, dictionary);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return Ok(responce);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="brannteknisk"></param>
+        /// <returns></returns>
+        [HttpPost, Route("BrannseksjonOgBrannmotstand")]
+        public IActionResult PostBB([FromBody] BranntekniskProsjekteringModel brannteknisk)
+        {
+            var key = "BrannseksjonOgBrannmotstand.Net";
+
+            var dictionary = brannteknisk.GetType()
+                .GetProperties(BindingFlags.Instance | BindingFlags.Public)
+                .ToDictionary(prop => prop.Name, prop => prop.GetValue(brannteknisk, null));
+            var camunda = new CamundaEngineClient();
+            var responce = camunda.BpmnWorkflowService.StartProcessInstance(key, dictionary);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return Ok(responce);
+        }
+        /// <summary>
+        /// Krav til branntiltak
+        /// </summary>
+        /// <param name="brannteknisk"></param>
+        /// <returns></returns>
+        [HttpPost, Route("KravTilBranntiltaktModel")]
+        public IActionResult PostKB([FromBody] BranntekniskProsjekteringModel brannteknisk)
+        {
+            var key = "KravTilBranntiltaktModel.Net";
+
+            var dictionary = brannteknisk.GetType()
+                .GetProperties(BindingFlags.Instance | BindingFlags.Public)
+                .ToDictionary(prop => prop.Name, prop => prop.GetValue(brannteknisk, null));
+            var camunda = new CamundaEngineClient();
+            var responce = camunda.BpmnWorkflowService.StartProcessInstance(key, dictionary);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return Ok(responce);
+        }
+        /// <summary>
+        /// Ledesystem
+        /// </summary>
+        /// <param name="brannteknisk"></param>
+        /// <returns></returns>
+        [HttpPost, Route("LedesystemModel")]
+        public IActionResult PostL([FromBody] BranntekniskProsjekteringModel brannteknisk)
+        {
+            var key = "LedesystemModel.Net";
+
+            var dictionary = brannteknisk.GetType()
+                .GetProperties(BindingFlags.Instance | BindingFlags.Public)
+                .ToDictionary(prop => prop.Name, prop => prop.GetValue(brannteknisk, null));
+            var camunda = new CamundaEngineClient();
+            var responce = camunda.BpmnWorkflowService.StartProcessInstance(key, dictionary);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return Ok(responce);
+        }
+        /// <summary>
+        /// Get model output
+        /// </summary>
         /// <param name="id"></param>
         /// <returns></returns>
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
+        [HttpGet("GetResult/{id}", Name = "Get")]
+        public IActionResult Get(string id)
         {
-            return "value";
+            var camunda = new CamundaEngineClient();
+            var responce = camunda.BpmnWorkflowService.GetProcessVariables(id);
+            if (responce != null && responce.Any())
+            {
 
-        }
-
-        // POST: api/DigiTek17K11
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT: api/DigiTek17K11/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+                var newDict = responce.Where(value => value.Key.Contains("modelOutputs") || value.Key.Contains("modelDataDictionary"))
+                    .ToDictionary(value => value.Key, value => value.Value);
+                return Ok(newDict);
+            }
+            return BadRequest(responce);
         }
     }
+
 }

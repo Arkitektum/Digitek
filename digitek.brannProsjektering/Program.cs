bb6d6ca6bde246347ae0d7c926dc776c219ac783
Henning Jensen <henning@arkitektum.no>
2018-10-15 12:46:33 +0200

    Add logging with serilog

--- digitek.brannProsjektering/Program.cs
+++ digitek.brannProsjektering/Program.cs
@@ -1,31 +1,96 @@
 ﻿using System;
+using System.IO;
 using System.Reflection;
 using CamundaClient;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Events;
 
 namespace digitek.brannProsjektering
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
+        {
+            var loggerConfiguration = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console();
+
+            var appConfiguration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile(
+                    $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json",
+                    optional: true)
+                .Build();
+
+            string logToFile = appConfiguration["Logging:LogToFile"];
+            if (!string.IsNullOrWhiteSpace(logToFile))
+            {
+                loggerConfiguration.WriteTo.File(logToFile,
+                    fileSizeLimitBytes: 1_000_000,
+                    rollOnFileSizeLimit: true,
+                    shared: true,
+                    flushToDiskInterval: TimeSpan.FromSeconds(1));
+            }
+
+            Log.Logger = loggerConfiguration.CreateLogger();
+
+            try
+            {
+                Log.Information("Deploying models to Camunda");
+                
+                DeployModelsToCamunda();
+                
+                Log.Information("Starting web host");
+
+                var host = BuildWebHost(args);
+
+                host.Run();
+
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        
+            
+        }
+
+        private static void DeployModelsToCamunda()
         {
             CamundaEngineClient camunda = new CamundaEngineClient();
             try
             {
                 var assemblyName = Assembly.GetExecutingAssembly().GetName().Name;
                 camunda.Startup(assemblyName); // Deploys all models to Camunda and Start all found ExternalTask-Workers
+                
+                Log.Information("Successfully deployed models to Camunda");
             }
             catch (Exception exception)
             {
-                Console.WriteLine("\n\n" + "Can't deploy to Server.\n\n");
+                Log.Error("Exception while deploying models to camunda: {message} ", exception.Message, exception);
             }
-
-            CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        private static IWebHost BuildWebHost(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .UseKestrel(c => c.AddServerHeader = false)
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .UseSerilog()
+                .Build();
+        }
     }
 }

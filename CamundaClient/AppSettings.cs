c0acd41362fce10c16ef6481bcd589d0d29fcac9
Henning Jensen <henning@arkitektum.no>
2018-11-23 13:55:23 +0100

    Add Camunda username and password to appsettings

--- CamundaClient/AppSettings.cs
+++ CamundaClient/AppSettings.cs
@@ -3,5 +3,7 @@ namespace digitek.brannProsjektering.Models
     public class AppSettings
     {
         public string CamundaUrl { get; set; }
+        public string CamundaUsername { get; set; }
+        public string CamundaPassword { get; set; }
     }
 }

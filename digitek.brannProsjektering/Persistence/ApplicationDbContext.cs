68c24db194e28d1d985c4f4805eaaea9bd9a1e20
Matias Gonzalez Tognoni <matias@arkitektum.no>
2019-01-18 13:23:21 +0100

    Db Context

---
+++ digitek.brannProsjektering/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using digitek.brannProsjektering.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace digitek.brannProsjektering.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<UseRecord> UseRecords { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options)
+        {
+
+        }
+    }
+}

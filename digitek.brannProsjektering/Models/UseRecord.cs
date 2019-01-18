26d09cff644288c8948b651f1fa598f60eb829b7
Matias Gonzalez Tognoni <matias@arkitektum.no>
2019-01-18 13:04:53 +0100

    Add model

---
+++ digitek.brannProsjektering/Models/UseRecord.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace digitek.brannProsjektering.Models
+{
+    public class UseRecord
+    {
+        public int Id { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Model { get; set; }
+        public string InputJson { get; set; }
+        public int ResponseCode { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}

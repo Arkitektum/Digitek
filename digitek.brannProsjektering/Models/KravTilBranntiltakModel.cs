de189d03c739bd8db628927763c5077ea10c8cbd
Matias Gonzalez Tognoni <matias@arkitektum.no>
2019-01-25 15:47:17 +0100

    Add variables to  KravTilBranntiltakModel

--- digitek.brannProsjektering/Models/KravTilBranntiltakModel.cs
+++ digitek.brannProsjektering/Models/KravTilBranntiltakModel.cs
@@ -8,6 +8,9 @@ namespace digitek.brannProsjektering.Models
     public class KravTilBranntiltakModel
     {
         public string rkl { get; set; }
-        public long? antallEtasjer { get; set; }
+        public string typeVirksomhet { get; set; }
+        public int? antallEtasjer { get; set; }
+        public bool? kravOmHeis { get; set; }
+        public int? brtArealBygg { get; set; }
     }
 }

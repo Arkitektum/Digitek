d6e1f7e03401c31cc4311219a82878f59a05929d
Matias Gonzalez Tognoni <matias@arkitektum.no>
2019-08-15 13:09:30 +0200

    Change TableInfo Name and path

--- digitek.brannProsjektering/Models/BrannDictionaryModel.cs
+++ digitek.brannProsjektering/Models/BrannDictionaryModel.cs
@@ -4,19 +4,6 @@ namespace digitek.brannProsjektering.Models
 {
     public class BrannDictionaryModel
     {
-        public Dictionary<string, TableInfo> BranntekniskProsjekteringDictionary { get; set; }
-    }
-
-    public class TableInfo
-    {
-        public string DmnId { get; set; }
-        public string DmnNavn { get; set; }
-        public string TekKapitel { get; set; }
-        public string TekLedd { get; set; }
-        public string TekTabell { get; set; }
-        public string TekForskriften { get; set; }
-        public string TekWebLink { get; set; }
-        public VariablesInfo[] InputVariablesInfo { get; set; }
-        public VariablesInfo[] OutputVariablesInfo { get; set; }
+        public Dictionary<string, DmnInfo> BranntekniskProsjekteringDictionary { get; set; }
     }
 }
---
+++ digitek.brannProsjektering/Models/DmnInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace digitek.brannProsjektering.Models
+{
+    public class DmnInfo
+    {
+        public string DmnId { get; set; }
+        public string DmnNavn { get; set; }
+        public string TekKapitel { get; set; }
+        public string TekLedd { get; set; }
+        public string TekTabell { get; set; }
+        public string TekForskriften { get; set; }
+        public string TekWebLink { get; set; }
+        public VariablesInfo[] InputVariablesInfo { get; set; }
+        public VariablesInfo[] OutputVariablesInfo { get; set; }
+    }
+}

ff176dd16d69185261d30bf9bd06ff8e01646715
Matias Gonzalez Tognoni <matias@arkitektum.no>
2019-08-19 12:37:40 +0200

    Update models

--- digitek.brannProsjektering/Models/VariablesInfo.cs
+++ digitek.brannProsjektering/Models/VariablesInfo.cs
@@ -8,9 +8,11 @@ namespace digitek.brannProsjektering.Models
     public class VariablesInfo
     {
         public string VariabelId { get; set; }
-        public string VariabelNavn { get; set; }
+        public string VariabelName { get; set; }
         public string VariabelBeskrivelse { get; set; }
-        public string Ifc { get; set; }
+        public string Ifc4 { get; set; }
         public string IfcUrl { get; set; }
+        public string VariabelType { get; set; }
+        public string VariabelUse { get; set; }
     }
 }
